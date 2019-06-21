bb4f0e45ab804bba065b530d29e1d409c7b42847
Soobinnn <isb9082@naver.com>
2019-06-21 21:09:00 +0900

    0621 board 진행중2

---
+++ pysite/board/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from user.models import User
+
+# Create your models here.
+
+
+class Board(models.Model):
+    board_no = models.AutoField(primary_key=True) # 자동증가, 기본키설정
+    title = models.CharField(max_length=100)
+    contents = models.TextField()  # 길이 제한 없는 문자열
+    hit = models.IntegerField()
+    reg_date = models.DateTimeField(auto_now=True)
+    group_no = models.IntegerField()
+    order_no = models.IntegerField()
+    depth = models.IntegerField()
+    # no = models.ForeignKey('User', on_delete=models.CASCADE)
+    def __str__(self):
+        return f'Board({self.board_no}, {self.title}, {self.contents}, {self.hit}, {self.reg_date}, {self.group_no}, {self.order_no}, {self.depth})'
